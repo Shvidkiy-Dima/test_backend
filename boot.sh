b56d70b056b4bf0f720365bfb39f3e52b63399d8
Dima <shvidkiy.dmitriy@gmail.com>
2020-10-08 20:40:12 +0300

    heroku deploing

--- boot.sh
+++ boot.sh
@@ -3,5 +3,5 @@
 python manage.py makemigrations
 python manage.py migrate --fake-initial
 python manage.py collectstatic --no-input
-python manage.py dockersuperuser
+python manage.py superuser_from_env
 exec python manage.py runserver 0.0.0.0:8000
